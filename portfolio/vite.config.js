b313d11253376f69cc88cf267d75824c26628d96
Sabikun Naher Islam Amrin <s.n.i.a00134@gmail.com>
2026-02-19 22:35:32 +0600

    Change base path in Vite config to root

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  base: '/Portfolio/',   // ✅ Add this line
+  base: '/',   // ✅ Add this line
   plugins: [vue()],
   resolve: {
     alias: {
